16568abf1ce4beb58aba9ed9bc284b01f6359466
Zexian Huang <zexianh@rdl-r5bx4s.desktop.cloud.unimelb.edu.au>
2023-11-21 20:05:35 +1100

    add dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -2,14 +2,14 @@ import torch
 import pathlib
 import pandas as pd
 
+from tqdm import tqdm
 from typing import Union
 from torch_geometric.data import Dataset, Data
 from torch_geometric.transforms import NormalizeScale
 from torch_geometric.utils import (negative_sampling, 
                                    add_random_edge, 
                                    dropout_node,
-                                   coalesce,
-                                   mask_feature)
+                                   coalesce)
     
 
 class PolygonDataset(Dataset):
@@ -60,3 +60,55 @@ class PolygonDataset(Dataset):
         if self.transform:
             data = self.transform(data)
         return data
+    
+    
+import numpy as np
+
+from typing import Any
+from shapely import wkt
+from DDSL.experiments.exp2_mnist.loader import poly2ve
+from DDSL.ddsl.ddsl import *
+
+class NUFTDataset(object):
+    def __init__(self, 
+                 dataset: Union[str, pathlib.Path, pd.DataFrame], 
+                 res: int = 16,
+                 embed_norm: Any = 'l2'):
+        super().__init__()   
+        # Dataframe
+        if isinstance(dataset, str or pathlib.Path):
+            df = pd.read_pickle(dataset) 
+        elif isinstance(dataset, pd.DataFrame):
+            df = dataset
+    
+        self.res = res
+        self.embed_norm = embed_norm
+        self.geoms = df.geom.tolist()
+        
+    def to_data_list(self):
+        data_list = []
+        
+        for id, geom in enumerate(tqdm(self.geoms)):
+            P = wkt.loads(geom)
+            V, E = poly2ve(P)
+            V += 1e-6*np.random.rand(*V.shape)
+            V = torch.tensor(V, dtype=torch.float64, requires_grad=False)
+            E = torch.LongTensor(E)
+            D = torch.ones(E.shape[0], 1, dtype=torch.float64)
+            
+            ddsl_spec = DDSL_spec((self.res, self.res), (1,1), 2)
+            F = ddsl_spec(V,E,D)
+            poly_nuft_embeds = F.flatten().to(V.dtype)
+            poly_nuft_embeds[poly_nuft_embeds.isnan()] = 0
+            
+            if self.embed_norm == None or self.embed_norm == "F":
+                poly_nuft_embeds_ = poly_nuft_embeds
+            elif self.embed_norm == "l2":
+                poly_nuft_embeds_norm = torch.norm(poly_nuft_embeds, p=2, dim=-1, keepdim=True)
+                poly_nuft_embeds_ = torch.div(poly_nuft_embeds, poly_nuft_embeds_norm)
+                
+            data = Data() 
+            data.x = poly_nuft_embeds_.view(1, -1).float()
+            data_list.append(data)
+            
+        return data_list
